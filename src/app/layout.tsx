4380f2fce5b705ee287321fdcf4069cdbb16fa22
thewanionly <pelloani@gmail.com>
2024-03-25 22:47:05 +0800

    refactor: fix formatting using `fiormat:fix` script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,16 +7,16 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Entertainment Web App',
   description:
-    'A web app that lets you search for your favourite movies and TV shows and bookmark them.'
+    'A web app that lets you search for your favourite movies and TV shows and bookmark them.',
 };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   );
