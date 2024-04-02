9f3c998ead04912b0ab600272ad06ab945d652d7
thewanionly <pelloani@gmail.com>
2024-04-03 03:54:39 +0800

    feat: import globals.css in storybook
    this is for tailwind styling to be reflected in storybook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next';
 
 import { outfit } from '@/lib/fonts';
-
-import './globals.css';
+import '@/styles/globals.css';
 
 export const metadata: Metadata = {
   title: 'Entertainment Web App',
