98fadd2b48bf346467975a8e6e6a7249a07a9921
thewanionly <pelloani@gmail.com>
2024-04-04 04:32:29 +0800

    feat: setup shadcn ui as component lbirary reference

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,10 @@
 import type { Config } from 'tailwindcss';
-import defaultTheme from 'tailwindcss/defaultTheme';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+const config = {
+  darkMode: ['class'],
+  content: ['./src/**/*.{ts,tsx}'],
+  prefix: '',
   theme: {
     colors: {
       red: '#FC4747',
@@ -15,13 +13,34 @@ const config: Config = {
       'greyish-blue': '#5A698F',
       white: '#FFFFFF',
     },
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       fontFamily: {
-        outfit: ['var(--font-outfit)', ...defaultTheme.fontFamily.sans],
+        outfit: ['var(--font-outfit)', ...fontFamily.sans],
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config;
 
 export default config;
