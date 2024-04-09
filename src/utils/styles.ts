b026f14dfdf7865f62cf7b51ee4b60d07d17bb07
thewanionly <pelloani@gmail.com>
2024-04-10 04:50:16 +0800

    feat: make custom fontSize work when using `cn`

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,5 +1,17 @@
 import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { extendTailwindMerge } from 'tailwind-merge';
+
+// Source: https://github.com/tailwindlabs/tailwindcss/discussions/12983#discussioncomment-8522228
+const twMerge = extendTailwindMerge({
+  // use the `extend` key in case you want to extend instead of override
+  override: {
+    classGroups: {
+      'font-size': [
+        { text: ['body-s', 'body-m', 'heading-xs', 'heading-s', 'heading-m', 'heading-l'] },
+      ],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
