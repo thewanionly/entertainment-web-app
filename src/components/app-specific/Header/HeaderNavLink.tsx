e3da2e20857e32db05a6781c3cd4bb5f47fc867f
thewanionly <pelloani@gmail.com>
2024-04-15 00:29:12 +0800

    refactor: transfer HeaderNavLink directly under Header folder
    remove HeaderNavLink story

--- src/components/app-specific/Header/HeaderNavLink.tsx
+++ src/components/app-specific/Header/HeaderNavLink.tsx
@@ -4,14 +4,15 @@ import * as AllIcons from '@/components/app-specific/Icon';
 import { IconButton, IconButtonSrLabel } from '@/components/generic/Button';
 import { cn } from '@/utils/styles';
 
-import { NavLinkType } from '../Header.constants';
-import { HeaderOrientation } from '../Header.types';
-import { getVerticalClasses } from '../Header.utils';
+import { NavLinkType } from './Header.constants';
+import { HeaderOrientation } from './Header.types';
+import { getVerticalClasses } from './Header.utils';
 
-type NavLinkProps = NavLinkType & {
+export type NavLinkProps = NavLinkType & {
   className?: string;
-  active: boolean;
+  active?: boolean;
   orientation?: HeaderOrientation;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => void;
 };
 
 export const HeaderNavLink = ({
@@ -21,6 +22,7 @@ export const HeaderNavLink = ({
   icon,
   active = false,
   orientation,
+  onClick,
 }: NavLinkProps) => {
   const NavIcon = AllIcons[icon as keyof typeof AllIcons];
 
@@ -32,12 +34,13 @@ export const HeaderNavLink = ({
   return (
     <IconButton
       className={cn(
-        'group flex h-4 p-0 text-greyish-blue hover:bg-transparent hover:text-red',
+        'hover:bg-transparent group flex h-4 p-0 text-greyish-blue hover:text-red',
         getVerticalClasses(orientation, 'iconButton'),
         active && 'text-white',
         className
       )}
       asChild
+      onClick={onClick}
     >
       <Link href={url}>
         {NavIcon && (
