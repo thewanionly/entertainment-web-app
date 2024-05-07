b61a17b8bc5ceb50d3462cc364c930a2af8ec268
thewanionly <pelloani@gmail.com>
2024-05-07 10:29:58 +0800

    refactor: add missing props for MediaCarouselCard in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,24 @@
 import Logo from 'public/logo.svg';
 
 import { MediaCarouselCard } from '@/components/app-specific/MediaCard';
+import { MediaCategory } from '@/components/app-specific/MediaCard/MediaCard.types';
+
+const mockMediaData = {
+  imgSrc: '/images/beyond-earth/trending/large.jpg',
+  imgAlt: 'A man wearing cold jacket standing in a rock with waters and cliff around',
+  title: `Beyond Earth`,
+  year: '2019',
+  category: MediaCategory.Movie,
+  rating: 'PG',
+  isBookmarked: false,
+};
 
 export default function Home() {
   return (
     <main>
       <h1>Entertainment Web App</h1>
       <Logo />
-
-      <MediaCarouselCard
-        imgSrc="/images/beyond-earth/trending/large.jpg"
-        imgAlt="A man wearing cold jacket standing in a rock with waters and cliff around"
-        title="Beyond Earth"
-      />
+      <MediaCarouselCard {...mockMediaData} />
     </main>
   );
 }
