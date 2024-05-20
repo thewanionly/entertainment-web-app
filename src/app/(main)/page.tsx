3c88d5d7014b3f45cf811cf0f97d8c84bd48e691
thewanionly <pelloani@gmail.com>
2024-05-21 04:03:59 +0800

    refactor: transfer TrendingSection under app/ui

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,5 @@
+import { TrendingSection } from '@/app/ui/TrendingSection';
 import { fetchTrendingMedias } from '@/services/medias/fetchTrendingMedias';
-
-import { TrendingSection } from './TrendingSection';
 
 export default async function Home() {
   const trendingMedias = await fetchTrendingMedias();
