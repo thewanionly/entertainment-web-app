3e3b251da696e487f284d4758aac3695c9e545f9
thewanionly <pelloani@gmail.com>
2024-06-01 23:10:49 +0800

    refactor: update grid gap accroding to designs

--- src/app/ui/TopRatedSection/TopRatedSection.tsx
+++ src/app/ui/TopRatedSection/TopRatedSection.tsx
@@ -22,7 +22,15 @@ export const TopRatedSection = ({ className = '', medias }: TopRatedSectionProps
       >
         {TOP_RATED_SECTION_HEADING}
       </h2>
-      <ul className="grid w-full grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4 xs:grid-cols-1">
+      <ul
+        className={cn(
+          'w-full',
+          'grid grid-cols-2 gap-4',
+          'xs:grid-cols-1',
+          'sm:grid-cols-3 sm:gap-x-[1.875rem] sm:gap-y-6',
+          'lg:grid-cols-4 lg:gap-x-10 lg:gap-y-8'
+        )}
+      >
         {medias.map(({ id, imagePath, title, releaseDate, mediaType }, index) => (
           <li key={id} data-testid="grid-item">
             <MediaGridCard
