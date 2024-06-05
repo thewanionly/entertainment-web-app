1bcccb6e9f9d6fa9379012c2eeca7fc725ceed9b
thewanionly <pelloani@gmail.com>
2024-06-06 06:27:57 +0800

    feat: add tmdb attribution

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import { Attribution } from '@/app/ui/Attribution';
 import { SearchSection } from '@/app/ui/SearchSection';
 import { Header } from '@/components/app-specific/Header';
 import '@/styles/globals.css';
@@ -8,12 +9,15 @@ export default function MainLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="max-w-limit lg:mx-auto lg:flex lg:pl-8">
+    <div className="max-w-limit lg:mx-auto lg:flex lg:flex-col lg:pl-8">
       <Header />
-      <main className="w-full pt-[56px] sm:pt-[128px] lg:min-w-0 lg:pl-[96px] lg:pt-0">
+      <main className="min-h-[85dvh] w-full pt-[56px] sm:pt-[128px] lg:min-w-0 lg:pl-[96px] lg:pt-0">
         <SearchSection />
         {children}
       </main>
+      <footer className="my-12 w-full lg:mb-16 lg:pl-[96px]">
+        <Attribution />
+      </footer>
     </div>
   );
 }
