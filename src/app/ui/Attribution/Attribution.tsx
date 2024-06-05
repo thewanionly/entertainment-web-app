09566fdf905d85e401c1639537ba9165bf45811e
thewanionly <pelloani@gmail.com>
2024-06-06 06:33:52 +0800

    feat: add "powered by" text

--- src/app/ui/Attribution/Attribution.tsx
+++ src/app/ui/Attribution/Attribution.tsx
@@ -3,13 +3,14 @@ import Link from 'next/link';
 
 export const Attribution = () => {
   return (
-    <div className="mx-auto flex w-[91.467%] flex-col items-center justify-center gap-5 text-center lg:mx-9">
+    <div className="mx-auto flex w-[91.467%] flex-col items-center justify-center gap-1 text-center lg:mx-9">
+      <span className="text-greyish-blue">Powered by</span>
       <Link href="https://www.themoviedb.org/" target="_blank">
         <div className="relative aspect-[7] w-[10vw] min-w-[6rem] max-w-[8rem]">
           <Image src="/tmdb-logo.svg" alt="TMDB logo" title="TMDB" fill />
         </div>
       </Link>
-      <p className="text-[11px] font-light text-white/75 sm:text-body-s">
+      <p className="mt-5 text-[11px] font-light text-white/75 sm:text-body-s">
         This product uses the{' '}
         <Link
           href="https://developer.themoviedb.org/docs"
