1f920509ec5e5b98e3bbc89e711b41593b640175
thewanionly <pelloani@gmail.com>
2024-06-16 04:00:37 +0800

    feat: show grid section in movie
    show popular movies by default

--- src/app/(main)/movies/page.tsx
+++ src/app/(main)/movies/page.tsx
@@ -1,5 +1,5 @@
+import { fetchPopularMovies } from '@/services/medias/fetchPopularMedias';
 import { fetchSearchMovieResults } from '@/services/medias/fetchSearchResults';
-import { cn } from '@/utils/styles';
 
 import { MediaGridSection } from '../_ui/MediaGridSection';
 
@@ -27,9 +27,7 @@ export default async function MoviesPage({
     );
   }
 
-  return (
-    <div className="mx-auto mb-10 mt-4 w-[91.467%] sm:mb-12 sm:mt-5 lg:mb-14 lg:mt-6 lg:w-full lg:px-9 2xl:pr-0">
-      <h1 className={cn('inline-block capitalize', 'text-heading-m sm:text-heading-l')}>Movies</h1>
-    </div>
-  );
+  const movies = await fetchPopularMovies();
+
+  return <MediaGridSection className="my-6 sm:my-[2.125rem]" title="Movies" medias={movies} />;
 }
