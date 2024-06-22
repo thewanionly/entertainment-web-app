3aafd364984f5616c735e2bd627155755932a595
thewanionly <pelloani@gmail.com>
2024-06-23 00:50:17 +0800

    refactor: remove sticky footer

--- src/components/app-specific/Footer/Footer.tsx
+++ src/components/app-specific/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { Attribution } from '../Attribution';
 
 export const Footer = () => {
   return (
-    <footer className="sticky bottom-0 z-30 w-full bg-dark-blue pb-8 pt-6 lg:pl-[96px]">
+    <footer className="my-12 w-full bg-dark-blue lg:mb-16 lg:pl-[96px]">
       <Attribution />
     </footer>
   );
