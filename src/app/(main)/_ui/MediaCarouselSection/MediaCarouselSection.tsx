640166528717792a4e6ce64930227c2f17be91a8
thewanionly <pelloani@gmail.com>
2024-06-23 19:02:42 +0800

    refactor: use MediaSection components for carousel sections

--- src/app/(main)/_ui/MediaCarouselSection/MediaCarouselSection.tsx
+++ src/app/(main)/_ui/MediaCarouselSection/MediaCarouselSection.tsx
@@ -4,23 +4,20 @@ import { Fragment, ReactNode } from 'react';
 
 import Link from 'next/link';
 
-import { MediaCarousel } from '@/components/app-specific/MediaCarousel';
+import { MediaSection } from '@/components/app-specific/MediaSection/MediaSection';
+import { MediaSectionCarousel } from '@/components/app-specific/MediaSection/MediaSectionCarousel';
+import { MediaSectionTitle } from '@/components/app-specific/MediaSection/MediaSectionTitle';
 import { CarouselOptions } from '@/components/generic/Carousel';
-import { useMediaQuery } from '@/hooks/useMediaQuery';
 import { Media } from '@/types/medias';
 import { cn } from '@/utils/styles';
 
 type MediaCarouselSectionProps = {
   className?: string;
   title: string;
-  titleClassName?: string;
   titleLink?: string;
   medias: Media[];
   carouselOptions?: CarouselOptions;
 };
-
-const SLIDES_TO_SCROLL_LG = 2;
-const SLIDES_TO_SCROLL_DEFAULT = 1;
 
 const TitleWrapper = ({ href, children }: { href?: string; children: ReactNode }) => {
   if (!href) {
@@ -37,39 +34,20 @@ const TitleWrapper = ({ href, children }: { href?: string; children: ReactNode }
 export const MediaCarouselSection = ({
   className = '',
   title,
-  titleClassName = '',
   titleLink,
   medias,
   carouselOptions,
 }: MediaCarouselSectionProps) => {
-  const lg = useMediaQuery('(min-width: 1024px)');
-
-  const defaultCarouselOptions: CarouselOptions = {
-    slidesToScroll: lg ? SLIDES_TO_SCROLL_LG : SLIDES_TO_SCROLL_DEFAULT,
-    skipSnaps: true,
-  };
-
   return (
-    <section className={cn('ml-auto w-[95.735%] overflow-hidden lg:w-full lg:pl-9', className)}>
+    <MediaSection
+      className={cn('ml-auto w-[95.735%] overflow-hidden lg:w-full lg:pl-9', className)}
+    >
       <TitleWrapper href={titleLink}>
-        <h2
-          className={cn(
-            'inline-block capitalize',
-            'mb-4 text-[1.25rem] font-light leading-[normal] tracking-[-0.019375rem]',
-            'sm:mb-[1.5625rem] sm:text-heading-l',
-            titleClassName
-          )}
-        >
+        <MediaSectionTitle className="mb-4 sm:mb-[1.5625rem] sm:text-heading-l">
           {title}
-        </h2>
+        </MediaSectionTitle>
       </TitleWrapper>
-      <MediaCarousel
-        items={medias}
-        options={{
-          ...defaultCarouselOptions,
-          ...carouselOptions,
-        }}
-      />
-    </section>
+      <MediaSectionCarousel medias={medias} options={carouselOptions} />
+    </MediaSection>
   );
 };
