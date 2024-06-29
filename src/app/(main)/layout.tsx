eb03246f9a4601a8805fed8f75edfc76d7f6e40d
thewanionly <pelloani@gmail.com>
2024-06-29 21:37:13 +0800

    feat: add padding bottom to main

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -9,7 +9,7 @@ export default function MainLayout({
   return (
     <div className="max-w-limit lg:mx-auto lg:flex lg:flex-col lg:pl-8">
       <Header />
-      <main className="min-h-[85dvh] w-full pt-[56px] sm:pt-[128px] lg:min-w-0 lg:pl-[96px] lg:pt-0">
+      <main className="min-h-[85dvh] w-full pb-6 pt-[56px] sm:pb-8 sm:pt-[128px] lg:min-w-0 lg:pl-[96px] lg:pt-0">
         {children}
       </main>
     </div>
