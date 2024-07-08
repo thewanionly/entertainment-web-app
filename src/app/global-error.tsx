f0db29818c12d704ed992090a66a1e5153c7a630
thewanionly <pelloani@gmail.com>
2024-07-09 07:05:05 +0800

    refactor: style global-error

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -2,6 +2,8 @@
 
 import { ErrorRecoveryButton } from '@/components/app-specific/ErrorSection/ErrorRecoveryButton';
 import { ErrorSection } from '@/components/app-specific/ErrorSection/ErrorSection';
+import { outfit } from '@/lib/fonts';
+import '@/styles/globals.css';
 
 export default function GlobalError({
   error,
@@ -11,9 +13,9 @@ export default function GlobalError({
   reset: () => void;
 }) {
   return (
-    <html>
-      <body>
-        <main>
+    <html lang="en" className={outfit.variable}>
+      <body className="bg-dark-blue text-body-s text-white">
+        <main className="max-w-limit lg:mx-auto">
           <ErrorSection error={error}>
             <ErrorRecoveryButton handleRecover={reset} />
           </ErrorSection>
