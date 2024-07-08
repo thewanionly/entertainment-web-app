6faa1b97ff7723157bbad7d18e862e5a86f73154
thewanionly <pelloani@gmail.com>
2024-07-09 06:52:22 +0800

    refactor: update styling of grid section skeleton's title

--- src/app/(main)/(media)/[media]/loading.tsx
+++ src/app/(main)/(media)/[media]/loading.tsx
@@ -1,5 +1,5 @@
 import { MediaGridSectionSkeleton } from '../_ui/MediaGridSectionSkeleton';
 
 export default function MediaPageLoading() {
-  return <MediaGridSectionSkeleton titleClassName="lg:mb-[2.375rem]" />;
+  return <MediaGridSectionSkeleton titleClassName="lg:mb-[2.375rem] min-w-[5rem] max-w-[10rem]" />;
 }
--- src/app/(main)/(media)/search/loading.tsx
+++ src/app/(main)/(media)/search/loading.tsx
@@ -1,5 +1,5 @@
 import { MediaGridSectionSkeleton } from '../_ui/MediaGridSectionSkeleton';
 
 export default function SearchPageLoading() {
-  return <MediaGridSectionSkeleton titleClassName="w-[50%]" />;
+  return <MediaGridSectionSkeleton titleClassName="w-[50%] xs:w-[80%]" />;
 }
