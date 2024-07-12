dad5b28e526f8753b5f2c8560ddcadb9b16fea3a
thewanionly <pelloani@gmail.com>
2024-07-13 04:22:25 +0800

    refactor: move bg gradient 2px down to address issue in mobile safari

--- src/components/app-specific/MediaModal/MediaModal.tsx
+++ src/components/app-specific/MediaModal/MediaModal.tsx
@@ -115,8 +115,8 @@ export const MediaModal = () => {
         <MediaCardImage
           className={cn(
             'aspect-[2] h-[unset] w-full',
-            'rounded-b-none rounded-t-md ',
-            'after:absolute after:bottom-0 after:left-0 after:h-[25%] after:w-full after:bg-gradient-to-b after:from-dark-blue/0 after:to-dark-blue/100'
+            'rounded-b-none rounded-t-md',
+            'after:absolute after:-bottom-0.5 after:left-0 after:h-[25%] after:w-full after:bg-gradient-to-b after:from-dark-blue/0 after:to-dark-blue/100'
           )}
           src={imagePath}
           alt={title}
