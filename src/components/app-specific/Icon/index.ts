26e08d6296feeab41c7df3a523a784ba67448b4c
thewanionly <pelloani@gmail.com>
2024-07-24 14:21:48 +0800

    perf: lazy load icons

--- src/components/app-specific/Icon/index.ts
+++ src/components/app-specific/Icon/index.ts
@@ -1,11 +1,13 @@
-export { default as About } from 'public/icons/icon-about.svg';
-export { default as BookMarkEmpty } from 'public/icons/icon-bookmark-empty.svg';
-export { default as BookMarkFull } from 'public/icons/icon-bookmark-full.svg';
-export { default as MediaTypeMovie } from 'public/icons/icon-media-type-movie.svg';
-export { default as MediaTypeTV } from 'public/icons/icon-media-type-tv.svg';
-export { default as NavBookmark } from 'public/icons/icon-nav-bookmark.svg';
-export { default as NavHome } from 'public/icons/icon-nav-home.svg';
-export { default as NavMovies } from 'public/icons/icon-nav-movies.svg';
-export { default as NavTVSeries } from 'public/icons/icon-nav-tv-series.svg';
-export { default as Play } from 'public/icons/icon-play.svg';
-export { default as Search } from 'public/icons/icon-search.svg';
+import dynamic from 'next/dynamic';
+
+export const About = dynamic(() => import('public/icons/icon-about.svg'));
+export const BookMarkEmpty = dynamic(() => import('public/icons/icon-bookmark-empty.svg'));
+export const BookMarkFull = dynamic(() => import('public/icons/icon-bookmark-full.svg'));
+export const MediaTypeMovie = dynamic(() => import('public/icons/icon-media-type-movie.svg'));
+export const MediaTypeTV = dynamic(() => import('public/icons/icon-media-type-tv.svg'));
+export const NavBookmark = dynamic(() => import('public/icons/icon-nav-bookmark.svg'));
+export const NavHome = dynamic(() => import('public/icons/icon-nav-home.svg'));
+export const NavMovies = dynamic(() => import('public/icons/icon-nav-movies.svg'));
+export const NavTVSeries = dynamic(() => import('public/icons/icon-nav-tv-series.svg'));
+export const Play = dynamic(() => import('public/icons/icon-play.svg'));
+export const Search = dynamic(() => import('public/icons/icon-search.svg'));
