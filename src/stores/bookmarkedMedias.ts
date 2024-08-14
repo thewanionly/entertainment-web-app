dfecf92573701db0fbf4cae0ab568743498b05f8
thewanionly <pelloani@gmail.com>
2024-08-14 14:55:35 +0800

    feat: add bookmarkedMedias store

---
+++ src/stores/bookmarkedMedias.ts
@@ -0,0 +1,29 @@
+import { create } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
+
+import { MediaCardType } from '@/types/medias';
+
+interface BookmarkedMediasState {
+  bookmarkedMedias: MediaCardType[];
+  addBookmarkedMedia: (media: MediaCardType) => void;
+  removeBookmarkedMedia: (id: MediaCardType['id']) => void;
+}
+
+export const useBookmarkedMediasStore = create<BookmarkedMediasState>()(
+  devtools(
+    persist(
+      (set) => ({
+        bookmarkedMedias: [],
+        addBookmarkedMedia: (media) =>
+          set((state) => ({ bookmarkedMedias: [...state.bookmarkedMedias, media] })),
+        removeBookmarkedMedia: (id) =>
+          set((state) => ({
+            bookmarkedMedias: state.bookmarkedMedias?.filter(
+              (bookmarkedMedia) => bookmarkedMedia.id !== id
+            ),
+          })),
+      }),
+      { name: 'boookmarkedMediasStore', partialize: (state) => state.bookmarkedMedias }
+    )
+  )
+);
