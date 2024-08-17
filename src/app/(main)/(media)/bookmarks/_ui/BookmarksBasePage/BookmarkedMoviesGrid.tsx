ce8c4f7f6dd104643147e050cdcc93918c138173
thewanionly <pelloani@gmail.com>
2024-08-18 04:17:01 +0800

    feat: show skeleton cards initially since localStorage is not in the server

--- src/app/(main)/(media)/bookmarks/_ui/BookmarksBasePage/BookmarkedMoviesGrid.tsx
+++ src/app/(main)/(media)/bookmarks/_ui/BookmarksBasePage/BookmarkedMoviesGrid.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import { MediaGridCardSkeleton } from '@/components/app-specific/MediaCard/MediaGridCard/MediaGridCardSkeleton';
 import {
   MediaSectionGrid,
   MediaSectionGridItems,
 } from '@/components/app-specific/MediaSection/MediaSectionGrid';
+import { useIsInClient } from '@/hooks/useIsInClient';
 import { useBookmarkedMediasStore } from '@/stores/bookmarkedMedias';
 import { MediaType } from '@/types/medias';
 
@@ -11,6 +13,19 @@ export const BookmarkedMoviesGrid = () => {
   const bookmarkedMovies = useBookmarkedMediasStore((state) =>
     state.bookmarkedMedias.filter(({ mediaType }) => mediaType === MediaType.MOVIE)
   );
+  const isInClient = useIsInClient();
+  const skeletonCards = Array.from({ length: 4 }, (_, index) => index);
+
+  if (!isInClient)
+    return (
+      <MediaSectionGrid>
+        {skeletonCards.map((skeletonCard) => (
+          <li key={skeletonCard}>
+            <MediaGridCardSkeleton />
+          </li>
+        ))}
+      </MediaSectionGrid>
+    );
 
   return (
     <MediaSectionGrid>
