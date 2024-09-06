0c9aa4ba49e762340d90d8439e226f392c2a6032
thewanionly <pelloani@gmail.com>
2024-09-06 14:39:38 +0800

    refactor: add suppor for label prop in Play buttons

--- src/components/app-specific/PlayButton/PlayButton.tsx
+++ src/components/app-specific/PlayButton/PlayButton.tsx
@@ -7,30 +7,27 @@ import { Play } from '../Icon';
 
 type PlayButtonProps = {
   className?: string;
+  label?: string;
 };
 
 export const PlayButton = forwardRef<HTMLButtonElement, PlayButtonProps & ButtonProps>(
-  ({ className = '', ...props }, ref) => {
-    const label = 'Play';
-
-    return (
-      <IconButton
-        className={cn(
-          'play-button',
-          'z-20 h-6 w-6 rounded-full bg-black/25 p-0 text-white shadow-md shadow-black/50',
-          'hover:bg-black/60 hover:text-white/80',
-          'motion-safe:transition-colors',
-          className
-        )}
-        title={label}
-        {...props}
-        ref={ref}
-      >
-        <Play className={cn('play-button-icon', 'h-6 w-6')} />
-        <IconButtonSrLabel label={label} />
-      </IconButton>
-    );
-  }
+  ({ className = '', label = 'Play', ...props }, ref) => (
+    <IconButton
+      className={cn(
+        'play-button',
+        'z-20 h-6 w-6 rounded-full bg-black/25 p-0 text-white shadow-md shadow-black/50',
+        'hover:bg-black/60 hover:text-white/80',
+        'motion-safe:transition-colors',
+        className
+      )}
+      title={label}
+      {...props}
+      ref={ref}
+    >
+      <Play className={cn('play-button-icon', 'h-6 w-6')} />
+      <IconButtonSrLabel label={label} />
+    </IconButton>
+  )
 );
 
 PlayButton.displayName = 'PlayButton';
--- src/components/app-specific/PlayButton/PlayButtonWithLabel.tsx
+++ src/components/app-specific/PlayButton/PlayButtonWithLabel.tsx
@@ -7,34 +7,31 @@ import { Play } from '../Icon';
 
 type PlayButtonProps = {
   className?: string;
+  label?: string;
 };
 
 export const PlayButton = forwardRef<HTMLButtonElement, PlayButtonProps & ButtonProps>(
-  ({ className = '', ...props }, ref) => {
-    const label = 'Play';
-
-    return (
-      <Button
-        variant="secondary"
-        className={cn(
-          'play-button-w-label',
-          'z-20 col-start-1 row-start-1 place-self-center',
-          'h-min gap-[15px] rounded-full p-[7px] text-body-m sm:gap-[19px] sm:p-[9px] sm:text-heading-xs',
-          'bg-white/25 text-white hover:bg-white/50 hover:text-dark-blue',
-          'motion-safe:transition-colors',
-          className
-        )}
-        title={label}
-        {...props}
-        ref={ref}
-      >
-        <Play
-          className={cn('play-button-w-label-icon', 'h-[25px] w-[25px] sm:h-[30px] sm:w-[30px]')}
-        />
-        <span className={cn('play-button-w-label-text', 'mr-[12px] sm:mr-[15px]')}>{label}</span>
-      </Button>
-    );
-  }
+  ({ className = '', label = 'Play', ...props }, ref) => (
+    <Button
+      variant="secondary"
+      className={cn(
+        'play-button-w-label',
+        'z-20 col-start-1 row-start-1 place-self-center',
+        'h-min gap-[15px] rounded-full p-[7px] text-body-m sm:gap-[19px] sm:p-[9px] sm:text-heading-xs',
+        'bg-white/25 text-white hover:bg-white/50 hover:text-dark-blue',
+        'motion-safe:transition-colors',
+        className
+      )}
+      title={label}
+      {...props}
+      ref={ref}
+    >
+      <Play
+        className={cn('play-button-w-label-icon', 'h-[25px] w-[25px] sm:h-[30px] sm:w-[30px]')}
+      />
+      <span className={cn('play-button-w-label-text', 'mr-[12px] sm:mr-[15px]')}>{label}</span>
+    </Button>
+  )
 );
 
 PlayButton.displayName = 'PlayButton';
