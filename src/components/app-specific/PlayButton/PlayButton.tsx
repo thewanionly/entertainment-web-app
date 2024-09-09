66a5d8dbe3f2e52254db17ab62fc4bb2b4dbdff1
thewanionly <pelloani@gmail.com>
2024-09-09 14:44:47 +0800

    feat: support stopping video

--- src/components/app-specific/PlayButton/PlayButton.tsx
+++ src/components/app-specific/PlayButton/PlayButton.tsx
@@ -1,5 +1,7 @@
 import { forwardRef } from 'react';
 
+import { CircleStop } from 'lucide-react';
+
 import { ButtonProps, IconButton, IconButtonSrLabel } from '@/components/generic/Button';
 import { cn } from '@/utils/styles';
 
@@ -7,27 +9,37 @@ import { Play } from '../Icon';
 
 type PlayButtonProps = {
   className?: string;
-  label?: string;
+  isPlaying: boolean;
+  playLabel?: string;
+  stopLabel?: string;
 };
 
 export const PlayButton = forwardRef<HTMLButtonElement, PlayButtonProps & ButtonProps>(
-  ({ className = '', label = 'Play', ...props }, ref) => (
-    <IconButton
-      className={cn(
-        'play-button',
-        'z-20 h-6 w-6 rounded-full bg-black/25 p-0 text-white shadow-md shadow-black/50',
-        'hover:bg-black/60 hover:text-white/80',
-        'motion-safe:transition-colors',
-        className
-      )}
-      title={label}
-      {...props}
-      ref={ref}
-    >
-      <Play className={cn('play-button-icon', 'h-6 w-6')} />
-      <IconButtonSrLabel label={label} />
-    </IconButton>
-  )
+  (
+    { className = '', isPlaying = false, playLabel = 'Play', stopLabel = 'Stop', ...props },
+    ref
+  ) => {
+    const label = isPlaying ? stopLabel : playLabel;
+    const Icon = isPlaying ? CircleStop : Play;
+
+    return (
+      <IconButton
+        className={cn(
+          'play-button',
+          'z-20 h-6 w-6 rounded-full bg-black/25 p-0 text-white shadow-md shadow-black/50',
+          'hover:bg-black/60 hover:text-white/80',
+          'motion-safe:transition-colors',
+          className
+        )}
+        title={label}
+        {...props}
+        ref={ref}
+      >
+        <Icon className={cn('play-button-icon', 'h-6 w-6')} />
+        <IconButtonSrLabel label={label} />
+      </IconButton>
+    );
+  }
 );
 
 PlayButton.displayName = 'PlayButton';
